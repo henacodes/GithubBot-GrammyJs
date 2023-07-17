986665da69d7839d341dae2cd4cd8a8bbe7c5b3e
Henok Ayenew <henacodes@gmail.com>
2023-07-17 14:33:28 +0400

    implemented user searching feature

--- bot.js
+++ bot.js
@@ -1,33 +1,46 @@
-import { Bot, session } from "grammy";
+import { Bot, session, InlineKeyboard, InlineQueryResultBuilder } from "grammy";
 import dotenv from "dotenv";
+import { Octokit } from "octokit";
+import axios from "axios";
 
 // utility functions and objects
-import { commands, initialState } from "./utills.js";
+import { commands, initialState, salute, searchUsers } from "./utills.js";
 
 // Load env variables
 dotenv.config();
 // connect to the bot
 const bot = new Bot(process.env.BOT_TOKEN);
+// Connect Github
+const octokit = new Octokit({
+  auth: process.env.GIT_AUTH,
+});
 
 bot.api.setMyCommands(commands);
 
 bot.use(session(initialState));
+
 bot.command("start", (ctx) => {
-  ctx.reply("Welcome to Github browser bot!\n what do you wanna do?", {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: "search repo", callback_data: "repo" },
-          { text: "search repo", callback_data: "repo" },
-        ],
-      ],
-      remove_keyboard: true,
-    },
+  salute(ctx);
+});
+
+bot.on("inline_query", async (ctx) => {
+  const query = ctx.inlineQuery.query;
+
+  const users = await searchUsers(octokit, query);
+  const results = users.map((user) => {
+    return InlineQueryResultBuilder.article(user.id, user.login, {
+      thumbnail_url: user.avatar_url,
+    }).text(`@${user.login}`);
   });
+
+  // Answer the inline query.
+  await ctx.answerInlineQuery(
+    results, // answer with result list
+    { cache_time: 30 * 24 * 3600 } // 30 days in seconds
+  );
 });
 
 bot.callbackQuery("repo", async (ctx) => {
-  ctx.deleteMessage();
   ctx.reply("repo search mode");
 });
 
--- utills.js
+++ utills.js
@@ -3,7 +3,89 @@ export const commands = [
   { command: "help", description: "Show help text" },
 ];
 
+export const salute = (ctx) => {
+  return ctx.reply("Welcome to Github browser bot!\n what do you wanna do?", {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: "search repo", callback_data: "repo" },
+          { text: "search user", callback_data: "user" },
+        ],
+      ],
+      remove_keyboard: true,
+    },
+  });
+};
+
 export const initialState = {
   user: false,
   repo: true,
+};
+
+export const fetchRepo = async (username, repo, octokit) => {
+  try {
+    const response = await octokit.request(
+      `GET /repos/${username}/${repo}/zipball/{ref}`,
+      {
+        owner: username,
+        repo: repo,
+        ref: "",
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const searchUsers = async (octokit, query) => {
+  const response = await octokit.rest.search.users({
+    q: query,
+  });
+
+  return response.data.items;
+};
+
+export const showUser = (
+  ctx,
+  name,
+  username,
+  bio,
+  img,
+  followers,
+  following
+) => {
+  ctx.telegram.sendPhoto(ctx.chat.id, img, {
+    caption: `${
+      name + ` (@${username} ) `
+    }\n${bio}\n${followers} followers\n${following} following
+  
+      `,
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+            text: "Repos",
+            callback_data: `repos ${username} `,
+          },
+        ],
+      ],
+    },
+  });
+};
+
+export const sendRepos = (ctx, repos, username) => {
+  ctx.telegram.sendMessage(
+    ctx.chat.id,
+    `Here the last 10 repos of @${username}`,
+    {
+      reply_markup: {
+        inline_keyboard: [
+          ...repos.map((repo) => [
+            { text: repo.name, callback_data: `repo ${username} ${repo.name}` },
+          ]),
+        ],
+      },
+    }
+  );
 };
